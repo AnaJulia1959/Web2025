37c53ebc5e478db279fd286af0069bcff7cc08e1
AnaJulia1959 <anajuliarodrigues1959@gmail.com>
2025-03-25 19:45:11 -0300

    commit2

---
+++ Trabalho de Aula/src/main/java/com/web22025/dto/ProfessorDTO.java
@@ -0,0 +1,6 @@
+package com.web22025.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record ProfessorDTO(@NotBlank String nome, @NotBlank String email) {
+}
---
+++ Trabalho de Aula/src/main/java/com/web22025/model/Curso.java
@@ -0,0 +1,64 @@
+
+package com.web22025.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="curso")
+public class Curso implements Serializable {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int ID;
+	@NotBlank
+	private String nome;
+	@NotBlank
+	private String descricao;
+	private LocalDate dataInicio;
+	private LocalDate dataFinal;
+	private String imagem;
+
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public LocalDate getDataInicio() {
+		return dataInicio;
+	}
+	public void setDataInicio(LocalDate dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+	public LocalDate getDataFinal() {
+		return dataFinal;
+	}
+	public void setDataFinal(LocalDate dataFinal) {
+		this.dataFinal = dataFinal;
+	}
+	public String getImagem() {
+		return imagem;
+	}
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+}
