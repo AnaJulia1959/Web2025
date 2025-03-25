b041d5a9b169ce130ac614f96f0b0d8872803c56
AnaJulia1959 <anajuliarodrigues1959@gmail.com>
2025-03-25 20:51:14 -0300

    aqruivos professor

--- Trabalho de Aula/src/main/java/com/web22025/controller/CategoriaController.java
+++ Trabalho de Aula/src/main/java/com/web22025/controller/CategoriaController.java
@@ -16,8 +16,11 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.web22025.dto.CategoriaDTO;
+import com.web22025.dto.ProfessorDTO;
 import com.web22025.model.Categoria;
+import com.web22025.model.Professor;
 import com.web22025.repository.CategoriaRepository;
+import com.web22025.repository.ProfessorRepository;
 
 import jakarta.validation.Valid;
 
@@ -93,6 +96,81 @@ public class CategoriaController {
 		msg.addFlashAttribute("sucesso", "Categoria editada!");
 		return "redirect:/categoria/listar";
 	}
+
+	@RequestMapping("/professor")
+	public class ProfessorController {
+    @Autowired
+    ProfessorRepository repository;
+    
+    @GetMapping("/inserir")
+    public String inserir() {
+        return "professor/inserir";
+    }
+    
+    @PostMapping("/inserir")
+    public String inserido(
+            @ModelAttribute @Valid ProfessorDTO dto,
+            BindingResult result, RedirectAttributes msg    
+            ) {
+        if(result.hasErrors()) {
+            msg.addFlashAttribute("erro","Erro ao inserir");
+            return "redirect:/professor/inserir";
+        }
+        var professor = new Professor();
+        BeanUtils.copyProperties(dto, professor);
+        repository.save(professor);
+        msg.addFlashAttribute("sucesso", "Professor cadastrado!");
+        return "redirect:/professor/inserir";
+    }
+    
+    @GetMapping("/listar")
+    public ModelAndView listar() {
+        ModelAndView mv = new ModelAndView("professor/listar");
+        List<Professor> lista = repository.findAll();
+        mv.addObject("professores", lista);
+        return mv;
+    }
+    
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable(value="id") int id) {
+        Optional<Professor> professor = repository.findById(id);
+        if(professor.isEmpty()) {
+            return "redirect:/professor/listar";
+        }
+        repository.deleteById(id);
+        return "redirect:/professor/listar";
+    }
+    
+    @GetMapping("/editar/{id}")
+    public ModelAndView editar(@PathVariable(value="id") int id) {
+        ModelAndView mv = new ModelAndView("professor/editar");
+        Optional<Professor> professor = repository.findById(id);
+        mv.addObject("id", professor.get().getID());
+        mv.addObject("nome", professor.get().getNome());
+        mv.addObject("email", professor.get().getEmail());
+        return mv;
+    }
+    
+    @PostMapping("/editar/{id}")
+    public String editado(
+            @ModelAttribute @Valid ProfessorDTO dto,
+            BindingResult result, RedirectAttributes msg,
+            @PathVariable(value="id") int id
+            ) {
+        
+        if(result.hasErrors()) {
+            msg.addFlashAttribute("erro","Erro ao editar");
+            return "redirect:/professor/inserir";
+        }
+        Optional<Professor> prof = repository.findById(id);
+
+        var professor = prof.get();
+        BeanUtils.copyProperties(dto, professor);
+        repository.save(professor);
+        msg.addFlashAttribute("sucesso", "Professor editado!");
+        return "redirect:/professor/listar";
+    }
+}
 }
 
 
---
+++ Trabalho de Aula/src/main/java/com/web22025/dto/ProfessorDTO.java
@@ -0,0 +1,11 @@
+package com.web22025.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record ProfessorDTO(
+    @NotBlank String nome,
+    @NotBlank @Email String email
+) {
+
+}
